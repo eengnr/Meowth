1aa26cc26d97dc82a0846f9221dfdb6bcf63e28e
eengnr <31403422+eengnr@users.noreply.github.com>
2019-06-03 21:19:10 +0200

    Add git commands

--- meowth/checks.py
+++ meowth/checks.py
@@ -14,7 +14,7 @@ def is_owner():
 
 def is_dev_check(ctx):
     author = ctx.author.id
-    dev_list = [132314336914833409, 174764205927432192, 263607303096369152]
+    dev_list = [391935860930772992]
     return author in dev_list
 
 def is_dev_or_owner():
---
+++ meowth/exts/dev.py
@@ -0,0 +1,35 @@
+import os
+from subprocess import Popen, PIPE, STDOUT
+from pathlib import Path
+
+from discord.ext import commands
+
+from meowth import utils
+from meowth import checks
+
+class DevTools:
+    def __init__(self, bot):
+        self.bot = bot
+
+    def __local_check(self, ctx):
+        return checks.is_dev_check(ctx)
+
+    @commands.group()
+    async def git(self, ctx):
+        ctx.git_path = Path(__file__).parent.parent.parent
+        ctx.git_cmd = ['git']
+
+    @git.command(name='status')
+    async def gitstatus(self, ctx):
+        ctx.git_cmd.append('status')
+        p = Popen(ctx.git_cmd, stdout=PIPE, stderr=STDOUT, cwd=ctx.git_path)
+        await ctx.codeblock(p.stdout.read().decode("utf-8"), syntax="")
+
+    @git.command(name='pull')
+    async def gitpull(self, ctx):
+        ctx.git_cmd.append('pull')
+        p = Popen(ctx.git_cmd, stdout=PIPE, stderr=STDOUT, cwd=ctx.git_path)
+        await ctx.codeblock(p.stdout.read().decode("utf-8"), syntax="")
+
+def setup(bot):
+    bot.add_cog(DevTools(bot))
